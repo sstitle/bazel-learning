032b43918de6c4c5b3c8c6588d3dfbe50bb93d13
Sam Title <samstitle@gmail.com>
2025-08-30 13:40:12 -0400

    Naive bazel

---
+++ flake.nix
@@ -0,0 +1,67 @@
+{
+  description = "Description for the project";
+
+  inputs = {
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+  };
+
+  outputs =
+    inputs@{ flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      imports = [
+        # To import a flake module
+        # 1. Add foo to inputs
+        # 2. Add foo as a parameter to the outputs function
+        # 3. Add here: foo.flakeModule
+
+      ];
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "aarch64-darwin"
+        "x86_64-darwin"
+      ];
+      perSystem =
+        {
+          config,
+          self',
+          inputs',
+          pkgs,
+          system,
+          ...
+        }:
+        {
+          # Per-system attributes can be defined here. The self' and inputs'
+          # module parameters provide easy access to attributes of the same
+          # system.
+
+          # Equivalent to  inputs'.nixpkgs.legacyPackages.hello;
+          packages.default = pkgs.hello;
+
+          devShells.default =
+            # ... with no globally-available CC toolchain ...
+            pkgs.mkShellNoCC {
+              name = "rules_nixpkgs_shell";
+
+              # ... which makes available the following dependencies,
+              # all sourced from the `pkgs` package set:
+              packages = with pkgs; [
+                bazel_7
+                bazel-buildtools
+                cacert
+                nix
+                git
+                nil # language server for Nix
+                starpls # language server for "Starlark" -> Bazel
+              ];
+            };
+        };
+      flake = {
+        # The usual flake attributes can be defined here, including system-
+        # agnostic ones like nixosModule and system-enumerating ones, although
+        # those are more easily expressed in perSystem.
+
+      };
+    };
+}
